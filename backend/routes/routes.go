3b42bb63cd50cddaa7060b5e9878d86e7da79dcc
azrilpramudia <azrilpramudia01@gmail.com>
2026-09-10 01:48:51 +0700

    feat: add get user, update, delete & cors

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -3,11 +3,19 @@ package routes
 import (
 	"github.com/azrilpramudia/go-restful-api/controllers"
 	"github.com/azrilpramudia/go-restful-api/middleware"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders: []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders: []string{"Content-Length"},
+	}))
 
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -19,6 +27,9 @@ func SetupRouter() *gin.Engine {
 	router.POST("/api/login", controllers.Login)
 	router.GET("/api/users", middleware.AuthMiddleware(), controllers.FindUsers)
 	router.POST("/api/users", middleware.AuthMiddleware(), controllers.CreateUser)
+	router.GET("/api/users/:id", middleware.AuthMiddleware(), controllers.FindUsersById)
+	router.PUT("/api/users/:id", middleware.AuthMiddleware(), controllers.UpdateUser)
+	router.DELETE("/api/users/:id", middleware.AuthMiddleware(), controllers.DeleteUser)
 
 	return router
 }
