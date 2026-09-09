d470270a2854d59d0734cb0a3cbd7a10fc57d40a
azrilpramudia <azrilpramudia01@gmail.com>
2026-09-10 02:23:08 +0700

    feat: add axios, tanstack query, boostrap

---
+++ frontend/src/hooks/auth/useLogin.tsx
@@ -0,0 +1,17 @@
+import { useMutation } from "@tanstack/react-query";
+
+import Api from "../../services/api";
+
+interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+export const useLogin = () => {
+  return useMutation({
+    mutationFn: async (data: LoginRequest) => {
+      const response = await Api.post("/api/login", data);
+      return response.data;
+    },
+  });
+};
---
+++ frontend/src/main.tsx
@@ -0,0 +1,20 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AuthProvider } from "./context/AuthContext.tsx";
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <AuthProvider>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </BrowserRouter>
+    </AuthProvider>
+  </StrictMode>,
+);
